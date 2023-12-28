d6029647290f36bbd300ebf009034436321d7c17
sujeet-kumar-lab49 <sujeet.kumar@iongroup.com>
2023-12-28 19:39:46 +0530

    spring-boot application

---
+++ pfm-core/src/main/java/com/lab49/data/pfm/core/PfmCoreApp.java
@@ -0,0 +1,13 @@
+package com.lab49.data.pfm.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfmCoreApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfmCoreApp.class, args);
+	}
+
+}
---
+++ pfm-core/src/test/java/com/lab49/data/pfm/core/PfManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab49.data.pfm.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
